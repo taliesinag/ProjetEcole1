7a0ff020549fa517482162edba1a5bc2febe84e1
Aymeric <aymericg.formation@gmail.com>
2018-04-12 12:17:14 +0200

    Creation de la base OK

--- src/main/java/BDD/PDO.java
+++ src/main/java/BDD/PDO.java
@@ -7,60 +7,86 @@ import java.sql.ResultSet;
 
 public class PDO {
 
-	private final static String URL = "jdbc:sqlite:C:/JavaWorkspaces/GretaWorkspace/ProjetEcole1/test.db";
+
+	private final static String URL = "jdbc:sqlite:test.db";
 
 	private static PDO pdo;
 
 	private Connection connexion;
-
+/**
+ * @return PDO
+ */
 	private PDO() {
 		try {
 			Class.forName("org.sqlite.JDBC");
 			connexion = DriverManager.getConnection(URL);
 
+
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
 
 	}
+/**
+ * 
+ * @param sql
+ * @return
+ */
+	private void create(String sql){
+		PreparedStatement stmt = null;
 
+		try {
+			stmt = connexion.prepareStatement(sql);
+			int result = stmt.executeUpdate();
+			
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("ERROR :"+e);
+		}
+
+
+	}
+	
 	private ResultSet sql(String sql) {
 		PreparedStatement stmt = null;
 
 		try {
 			stmt = connexion.prepareStatement(sql);
-
-			return stmt.executeQuery();
+			ResultSet result = stmt.executeQuery();
+			return result;
 
 		} catch (Exception e) {
 			// TODO: handle exception
-			System.out.println(e);
+			System.out.println("ERROR :"+e);
 		}
 
 		return null;
 	}
 
-	/**
-	 * Méthode de Test pour la classe.
-	 * 
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		PDO.init();
-	}
 
 	/**
 	 * Initialisation du singleton qui gère la connexion à la base de données
 	 * sqlite.
+	 * 
+	 * @return Void
 	 */
 	public static void init() {
 		pdo = new PDO();
-		System.out.println(pdo.sql("CREATE TABLE IF NOT EXISTS Usager(id INTEGER, nom VARCHAR)"));
+		pdo.create("CREATE TABLE IF NOT EXISTS Usager(Id INTEGER PRIMARY KEY Autoincrement,Nom_Usager Varchar (50) NOT NULL);");
+		pdo.create("CREATE TABLE IF NOT EXISTS Livre(Id INTEGER PRIMARY KEY Autoincrement,Titre_livre Varchar (25) NOT NULL ,Annee Date NOT NULL ,Nom_Aut Varchar (50) NOT NULL ,Prenom_Aut Varchar (50) ,Nom_Edit Varchar (50) NOT NULL ,Prenom_Edit Varchar (50) NOT NULL);");
+		pdo.create("CREATE TABLE Emprunt (Id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, Date_Emprunt NUMERIC NOT NULL, Date_Rendu NUMERIC NOT NULL, Id_Usager INTEGER NOT NULL REFERENCES Livre (Id), Id_Livre INTEGER NOT NULL REFERENCES Livre (Id), FOREIGN KEY (Id_Usager) REFERENCES Usager (Id), FOREIGN KEY (Id_Livre) REFERENCES Livre (Id));");
+		//pdo.sql("ALTER TABLE Emprunt ADD CONSTRAINT IF NOT EXIST FK_Emprunt_Id_Usager FOREIGN KEY (Id_Usager) REFERENCES Usager(Id);");
+		//pdo.sql("ALTER TABLE Emprunt ADD CONSTRAINT IF NOT EXIST FK_Emprunt_Id_Livre FOREIGN KEY (Id_Livre) REFERENCES Livre(Id);");
 	}
 	
-	public static ResultSet Sql(String sql){
-		System.out.println(sql);
-		return null;
+	/**
+	 * 
+	 * @param sql
+	 * @return
+	 */
+	public ResultSet Sql(String sql){
+		return sql(sql);
 	}
 
 }
--- src/test/java/IAA/ProjetEcole1/PDOtest.java
+++ src/test/java/IAA/ProjetEcole1/PDOtest.java
@@ -1,5 +1,16 @@
 package IAA.ProjetEcole1;
 
+import BDD.PDO;
+
 public class PDOtest {
 
+
+	/**
+	 * Méthode de Test pour la classe.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		PDO.init();
+	}
 }
